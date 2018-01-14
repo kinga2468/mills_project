6316f43e504ec3e5ea4c3ce3523340a7dce74a6b
kinga2468 <kinga.bozecka@op.pl>
2018-01-14 20:04:21 +0100

    14.01 - 20.03
    wygląd planszy jest zrobiony, do poprawki tylko wygląd przy responsywności

--- static/js/jquery.js
+++ static/js/jquery.js
@@ -1,5 +1,24 @@
 var p0 = document.getElementById('p0');
+var p1 = document.getElementById('p1');
+var p2 = document.getElementById('p2');
+var p3 = document.getElementById('p3');
+var p4 = document.getElementById('p4');
+var p5 = document.getElementById('p5');
+var p6 = document.getElementById('p6');
+var p7 = document.getElementById('p7');
+var p8 = document.getElementById('p8');
+
 p0.addEventListener("click", function () { add_pawn(0) } );
+p1.addEventListener("click", function () { add_pawn(1) } );
+p2.addEventListener("click", function () { add_pawn(2) } );
+p3.addEventListener("click", function () { add_pawn(3) } );
+p4.addEventListener("click", function () { add_pawn(4) } );
+p5.addEventListener("click", function () { add_pawn(5) } );
+p6.addEventListener("click", function () { add_pawn(6) } );
+p7.addEventListener("click", function () { add_pawn(7) } );
+p8.addEventListener("click", function () { add_pawn(8) } );
+
+/*$(p0).on('click', function () { add_pawn(0) } );*/
 
 function add_pawn(nr) {
     alert(nr);
